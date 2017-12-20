0720a1e9770feb73a07e4e81d13012107ad71ef9
mfkasim91 <firman.kasim@gmail.com>
2017-12-20 18:14:32 +0000

    addpath(genpath(pwd))

--- main_forward.m
+++ main_forward.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Predict the intensity on the target plane given the deflection potential, Phi, and 
+% Predict the intensity on the target plane given the deflection potential, Phi, and
 %   the intensity profile on the source plane, sourceMap.
 % The beam from source plane is mapped to the target plane as function of its position on the source plane.
 % The map is given by:
@@ -16,24 +16,26 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function targetMap = main_forward(sourceMap, Phi)
+    addpath(genpath(pwd));
+    
     % getting the basic parameters for this algorithm
     [Ny, Nx] = size(sourceMap);
     [x,y] = meshgrid([1:Nx], [1:Ny]);
     maxIntensity = max(sourceMap(:)) * 1000;
-    
+
     % obtain the deflection angles
     alphax = -diff(Phi, [], 2);
     alphay = -diff(Phi, [], 1);
     alphax(:,Nx) = alphax(:,Nx-1); % just to make it the same size
     alphay(Ny,:) = alphay(Ny-1,:);
-    
+
     % get the mapping plan
     xmap = x + alphax;
     ymap = y + alphay;
-    
+
     I = zeros(size(x));
-    times = zeros([1 7]); % optim: 
-    
+    times = zeros([1 7]); % optim:
+
     % start the algorithm here
     tic;
     [Nymap, Nxmap] = size(xmap);
@@ -41,7 +43,7 @@ function targetMap = main_forward(sourceMap, Phi)
     xB = xmap(2:Nymap  , 1:Nxmap-1); yB = ymap(2:Nymap  , 1:Nxmap-1);
     xC = xmap(2:Nymap  , 2:Nxmap  ); yC = ymap(2:Nymap  , 2:Nxmap  );
     xD = xmap(1:Nymap-1, 2:Nxmap  ); yD = ymap(1:Nymap-1, 2:Nxmap  );
-    
+
     % sort polygons to have cw representations
     Ntot = prod(size(xA));
     xpoly = [reshape(xA, [1, Ntot]);     ...
@@ -52,7 +54,7 @@ function targetMap = main_forward(sourceMap, Phi)
              reshape(yB, [1, Ntot]); ...
              reshape(yC, [1, Ntot]); ...
              reshape(yD, [1, Ntot])];
-    
+
     xcentre = repmat(mean(xpoly,1), [4,1]);
     ycentre = repmat(mean(ypoly,1), [4,1]);
     dxpoly = xpoly - xcentre;
@@ -62,53 +64,53 @@ function targetMap = main_forward(sourceMap, Phi)
     idxsort = idxsort + size(idxsort,1) * repmat([0:size(idxsort,2)-1], [size(idxsort,1), 1]);
     xparallelogram = xpoly(idxsort);
     yparallelogram = ypoly(idxsort);
-    
+
     % get the area of the parallelograms in format:
     % [dS(0); dS(1); ...]
     dS = polyarea(xparallelogram, yparallelogram)';
-    
+
     % get the normalised coordinate in format:
     % [xA0, xB0, xC0, xD0; xA1, xB1, xC1, xD1; ...];
     ixparallelogram = (xparallelogram - x(1,1))' + 1;
     iyparallelogram = (yparallelogram - y(1,1))' + 1;
-    
+
     % get the I0 in form of [I0linear(1); I0linear(2), ...];
     I0linear = reshape(sourceMap(1:Nymap-1, 1:Nxmap-1), [(Nymap-1)*(Nxmap-1), 1]);
     I0centre = I0linear ./ dS;
     dt = toc;
     times(1) = times(1) + dt;
-    
+
     for (i = [1:(Nymap-1)*(Nxmap-1)])
         % if (mod(i-1, (Nxmap-1)) == 0) disp((i-1) / (Nxmap-1) + 2); end;
-        
+
         tic;
         % get the pixels enclosed by the polygon
         [ixl, iyl, ixp, iyp] = pixels_enclosed_by_polygon(ixparallelogram(i,:), iyparallelogram(i,:));
         dt = toc;
         times(2) = times(2) + dt;
-        
+
         % iterate for all pixels enclosed by polygon
         tic;
         for (ip = [1:length(ixp)])
             ix = ixp(ip);
             iy = iyp(ip);
-            
+
             % check the range
             if ((iy < 1) || (iy > size(y,1))) continue; end;
             if ((ix < 1) || (ix > size(x,2))) continue; end;
-            
+
             I(iy,ix) = I(iy,ix) + I0centre(i);
-            
+
         end
         dt = toc;
         times(3) = times(3) + dt;
-        
+
         % iterate for all pixels crossed by polygon's edges
         for (il = [1:length(ixl)])
             tic;
             ix = ixl(il);
             iy = iyl(il);
-            
+
             % check the range
             if ((iy < 1) || (iy > size(y,1))) continue; end;
             if ((ix < 1) || (ix > size(x,2))) continue; end;
@@ -117,7 +119,7 @@ function targetMap = main_forward(sourceMap, Phi)
             yvox = y(iy,1) + [0;1;1;0];
             dt = toc;
             times(4) = times(4) + dt;
-            
+
             if (dS ~= 0)
                 tic;
                 [xpoly, ypoly] = clip_polygons_with_rect(xvox, yvox, xparallelogram(:,i), yparallelogram(:,i));
@@ -135,16 +137,16 @@ function targetMap = main_forward(sourceMap, Phi)
             end
         end
     end
-    
+
     % clip the intensity
     I(I > maxIntensity) = maxIntensity;
-    
+
     % copy the intensity at the boundary
     I(:,Nx) = I(:,Nx-1);
     I(Ny,:) = I(Ny-1,:);
-    
+
     % assign the output
     targetMap = I;
-    
+
     % I = times; % optim
 end
